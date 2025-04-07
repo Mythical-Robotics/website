cab4e9d87b6f39c80d24617820c0c32105b2699b
Emilio Alvarez <e.l.alvarez@icloud.com>
2025-04-07 18:32:23 -0500

    inital comit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import MRlogo from './assets/Mythical Robotics.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://mythicalrobotics.org" target="_blank">
+          <img src={MRlogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Mythical Robotics</h1>
+      <div className="card">
+        <p>
+          Our site is under constrction. Come back soon!
+        </p>
+      </div>
+      <p className="read-the-docs">
+        A 501(c)(3) Noprofit organization.
+      </p>
+    </>
+  )
+}
+
+export default App
